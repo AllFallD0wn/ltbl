5503ca33e6b8f226d0f8157c9f31dcdbdb51ddc6
AllFallD0wn <nickcase@outlook.com>
2015-04-02 10:25:36 +0100

    Added properties to IDevice and NetworkType enum

--- Base/IDevice.cs
+++ Base/IDevice.cs
@@ -7,5 +7,13 @@ namespace Base
 {
     public interface IDevice
     {
+        //Name of the device
+        string Name { get; set; }
+        //IP Address of the device
+        string Address { get; set; }
+        //Port of the device
+        int Port { get; set; }
+        //Type of packet to send
+        NetworkType Type { get; set; }
     }
 }
